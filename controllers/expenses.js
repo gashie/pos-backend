7a925e5c59b1b3c6c9a3ecebf1595529ff8376fe
gashie <richardwilliam60@gmail.com>
2023-10-31 13:07:18 +0000

    add expense controllers

---
+++ controllers/expenses.js
@@ -0,0 +1,95 @@
+const asynHandler = require("../middleware/async");
+const { sendResponse, CatchHistory } = require("../helper/utilfunc");
+const GlobalModel = require("../model/Global")
+const systemDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+exports.CreateExpensesCategory = asynHandler(async (req, res, next) => {
+    //check if supplier exist for tenant
+    let userData = req.user;
+    let tenant_id = userData?.tenant_id
+    let payload = req.body;
+    payload.tenant_id = tenant_id
+    let results = await GlobalModel.Create(payload, 'expense_category','');
+    if (results.rowCount == 1) {
+        CatchHistory({ payload: JSON.stringify(payload), api_response: `New expense_category added`, function_name: 'CreateExpensesCategory', date_started: systemDate, sql_action: "INSERT", event: "Create Expense category item", actor: userData.id }, req)
+        return sendResponse(res, 1, 200, "Record saved", [])
+    } else {
+        CatchHistory({ payload: JSON.stringify(payload), api_response: `Sorry, error saving record: contact administrator`, function_name: 'CreateExpensesCategory', date_started: systemDate, sql_action: "INSERT", event: "Create Expense category item", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+    }
+
+})
+exports.ViewTenantExpensesCategory = asynHandler(async (req, res, next) => {
+    let userData = req.user;
+    let tenant_id = userData?.tenant_id
+    let results = await GlobalModel.Find('tenant_id', tenant_id, 'expense_category');
+    if (results.rows.length == 0) {
+        CatchHistory({ api_response: "No Record Found", function_name: 'ViewTenantExpensesCategory', date_started: systemDate, sql_action: "SELECT", event: "View expense category items", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Sorry, No Record Found", [])
+    }
+    CatchHistory({ api_response: `User with ${userData.id} viewed ${results.rows.length} expense category record's`, function_name: 'ViewTenantExpensesCategory', date_started: systemDate, sql_action: "SELECT", event: "View expense category items", actor: userData.id }, req)
+
+    sendResponse(res, 1, 200, "Record Found", results.rows)
+})
+exports.UpdateExpenseCategory = asynHandler(async (req, res, next) => {
+    let payload = req.body;
+    let userData = req.user;
+    payload.updated_at = systemDate
+
+    const runupdate = await GlobalModel.Update(payload, 'expense_category', 'category_id', payload.category_id)
+    if (runupdate.rowCount == 1) {
+        CatchHistory({ payload: JSON.stringify(req.body), api_response: `User with id :${userData.id} updated expense category details`, function_name: 'UpdateExpenseCategory', date_started: systemDate, sql_action: "UPDATE", event: "Update expense category items", actor: userData.id }, req)
+        return sendResponse(res, 1, 200, "Record Updated",runupdate.rows[0])
+
+
+    } else {
+        CatchHistory({ payload: JSON.stringify(req.body), api_response: `Update failed, please try later-User with id :${userData.id} updated expense category details`, function_name: 'UpdateExpenseCategory', date_started: systemDate, sql_action: "UPDATE", event: "Update expense category items", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Update failed, please try later", [])
+    }
+})
+exports.AddExpenseData = asynHandler(async (req, res, next) => {
+    //check if supplier exist for tenant
+    let userData = req.user;
+    let tenant_id = userData?.tenant_id
+    let payload = req.body;
+    payload.tenant_id = tenant_id
+    let results = await GlobalModel.Create(payload, 'expenses','');
+    if (results.rowCount == 1) {
+        CatchHistory({ payload: JSON.stringify(payload), api_response: `New expenses data added`, function_name: 'AddExpenseData', date_started: systemDate, sql_action: "INSERT", event: "Create expense data item", actor: userData.id }, req)
+        return sendResponse(res, 1, 200, "Record saved", [])
+    } else {
+        CatchHistory({ payload: JSON.stringify(payload), api_response: `Sorry, error saving record: contact administrator`, function_name: 'AddExpenseData', date_started: systemDate, sql_action: "INSERT", event: "Create expense data item", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Sorry, error saving record: contact administrator", [])
+
+    }
+
+})
+exports.ViewTenantExpenseData = asynHandler(async (req, res, next) => {
+    let userData = req.user;
+    let tenant_id = userData?.tenant_id
+    let results = await GlobalModel.Find('tenant_id', tenant_id, 'expenses');
+    if (results.rows.length == 0) {
+        CatchHistory({ api_response: "No Record Found", function_name: 'ViewTenantExpenseData', date_started: systemDate, sql_action: "SELECT", event: "View expense data items", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Sorry, No Record Found", [])
+    }
+    CatchHistory({ api_response: `User with ${userData.id} viewed ${results.rows.length} expense data record's`, function_name: 'ViewTenantExpenseData', date_started: systemDate, sql_action: "SELECT", event: "View expense data items", actor: userData.id }, req)
+
+    sendResponse(res, 1, 200, "Record Found", results.rows)
+})
+exports.UpdateExpenseData= asynHandler(async (req, res, next) => {
+    let payload = req.body;
+    let userData = req.user;
+    payload.updated_at = systemDate
+
+    const runupdate = await GlobalModel.Update(payload, 'expenses', 'expense_id', payload.expense_id)
+    if (runupdate.rowCount == 1) {
+        CatchHistory({ payload: JSON.stringify(req.body), api_response: `User with id :${userData.id} updated expense data details`, function_name: 'UpdateExpenseData', date_started: systemDate, sql_action: "UPDATE", event: "Update expense data items", actor: userData.id }, req)
+        return sendResponse(res, 1, 200, "Record Updated",runupdate.rows[0])
+
+
+    } else {
+        CatchHistory({ payload: JSON.stringify(req.body), api_response: `Update failed, please try later-User with id :${userData.id} updated expense data details`, function_name: 'UpdateExpenseData', date_started: systemDate, sql_action: "UPDATE", event: "Update income category items", actor: userData.id }, req)
+        return sendResponse(res, 0, 200, "Update failed, please try later", [])
+    }
+})
